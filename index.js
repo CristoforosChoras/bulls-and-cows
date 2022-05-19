f62e587cdf97e6e00da566869abbd538543a3787
CristoforosChoras <christopher.choras@yahoo.com>
2022-05-19 15:59:51 +0300

    creating the random number

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 // Bulls and Cows
 // Get library for user input
 // we need to keep the next line, so we can prompt the user for input
-const prompt = require('prompt-sync')({ sigint: true });
+// const prompt = require("prompt-sync")({ sigint: true });
 
-// Test that prompt is working 
-let name = prompt('What is your name? ');
-console.log(`User's input is: ${name}`);
+// Test that prompt is working
+// let name = prompt("What is your name? ");
+// console.log(`User's input is: ${name}`);
 
- // Feel free to edit / remove the line above, this is just to test the package
-//  Although we may want to use the user's name for something 
+
+// creating the random number 
+let numberPick = [1,2,3,4,5,6,7,8,9]
+
+numberPick.sort(()=> (Math.random() -0.5))
+
+console.log(numberPick);
+
+
+
+
+// Feel free to edit / remove the line above, this is just to test the package
+//  Although we may want to use the user's name for something
